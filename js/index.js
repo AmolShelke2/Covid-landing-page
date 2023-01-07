db86ecc1397df52e8b8d1e2513ab2aba4a3dcbdb
Amol shelke <shelkeamol897@gmail.com>
2023-01-08 01:35:58 +0530

    Added filter Challenge

--- js/index.js
+++ js/index.js
@@ -1,13 +1,45 @@
-const button = document.querySelector(".navbar__ham");
-const menu = document.querySelector(".navbar__links");
+// const button = document.querySelector(".navbar__ham");
+// const menu = document.querySelector(".navbar__links");
 
-button.addEventListener("click", () => {
-  button.classList.toggle("open");
-  menu.classList.toggle("navbar__open");
+// button.addEventListener("click", () => {
+//   button.classList.toggle("open");
+//   menu.classList.toggle("navbar__open");
+// });
+
+let numArray = [10, 8, 6, 5];
+let numLessThanTen = numArray.filter((element, index, arr) => {
+  console.log(element, index, arr);
 });
 
-let numArray = [10, 8, 6, 5, 4, 2, 1, 18, 12];
-let numLessThanTen = numArray.filter((num, index, arr) => {
-  console.log(num, index, arr);
+let customers = [
+  {
+    name: "Naruto",
+    address: "Konohagakure",
+    payIsOnline: true,
+  },
+  {
+    name: "Sasuke",
+    address: "KonohaGakure",
+    payIsOnline: false,
+  },
+  {
+    name: "Sakura",
+    address: "KonohaGakure",
+    payIsOnline: false,
+  },
+  {
+    name: "Hinata",
+    address: "KonohaGakure",
+    payIsOnline: true,
+  },
+];
+
+const specialDiscountUsers = customers.filter(
+  (customer) => customer.payIsOnline
+);
+
+const specialDiscountUsers2 = customers.filter(function (customer) {
+  return customer.payIsOnline;
 });
-console.log(numLessThanTen);
+
+console.log(specialDiscountUsers);
