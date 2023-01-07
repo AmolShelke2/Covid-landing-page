36eefee07120c64cae71093d4b2be8c906afeb7d
Amol shelke <shelkeamol897@gmail.com>
2023-01-07 23:29:26 +0530

    added navbar logic

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-// const button = document.querySelector(".navbar__ham");
-// const menu = document.querySelector(".navbar__links");
+const button = document.querySelector(".navbar__ham");
+const menu = document.querySelector(".navbar__links");
 
-// button.addEventListener("click", () => {
-//   button.classList.toggle("open");
-//   menu.classList.toggle("navbar__open");
-// });
+button.addEventListener("click", () => {
+  button.classList.toggle("open");
+  menu.classList.toggle("navbar__open");
+});
 
 let numArray = [10, 8, 6, 5, 4, 2, 1, 18, 12];
 let numLessThanTen = numArray.filter((num, index, arr) => {
