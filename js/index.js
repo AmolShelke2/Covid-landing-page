b8ce5bd41f36989cadd559e7cf9930863b63240d
Amol shelke <shelkeamol897@gmail.com>
2023-01-07 23:29:00 +0530

    Added a filter method example

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
-const button = document.querySelector(".navbar__ham");
-const menu = document.querySelector(".navbar__links");
+// const button = document.querySelector(".navbar__ham");
+// const menu = document.querySelector(".navbar__links");
 
-button.addEventListener("click", () => {
-  button.classList.toggle("open");
-  menu.classList.toggle("navbar__open");
+// button.addEventListener("click", () => {
+//   button.classList.toggle("open");
+//   menu.classList.toggle("navbar__open");
+// });
+
+let numArray = [10, 8, 6, 5, 4, 2, 1, 18, 12];
+let numLessThanTen = numArray.filter((num, index, arr) => {
+  console.log(num, index, arr);
 });
+console.log(numLessThanTen);
